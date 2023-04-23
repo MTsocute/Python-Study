da1f357029e5a348ed2287fc0da0c42e32590f61
Momo <75737534+MTsocute@users.noreply.github.com>
2023-04-23 18:01:46 +0800

    Chapter 2

---
+++ Chapter2/字符串.py
@@ -0,0 +1,25 @@
+name = "ada wang"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+
+# 字符串的合并
+name1 = "Mo"
+name2 = "mo"
+
+my_name = name1 + name2
+print(my_name)
+print(my_name + " Love " + name.upper())
+
+# 删除空白结尾空白
+name4 = "Good Boy!! "
+print(name4.rstrip())  # 删除 right 的空白，
+
+# 同理有 lstrip() 和 strip()
+# 删除左边和两端都删除
+name5 = " Fuck YOU "
+print(name5)
+print(name5.lstrip())
+print(name5.strip())
+
---
+++ Chapter2/数字.py
@@ -0,0 +1,14 @@
+# 除法运算
+a = 12
+b = 5
+print(a / b)    # 这个和 C 是有很大差别的，因为 C 严格区分变量类型
+
+# 乘方运算
+print(2**2)
+
+# python 的变量类型转换
+age = 23
+# print("Happy" + age + "rd Birthday")    # 这个是错的，因为字符串连接之后要保持类型一致
+# 所以我们要转换数字类型为字符串类型
+print("Happy " + str(age) + "rd Birthday")
+
